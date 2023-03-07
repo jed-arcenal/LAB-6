54e74d7a8cdb6f932ccdfdf07bd3d852ed56401f
JArsenal1 <95877015+JArsenal1@users.noreply.github.com>
2023-03-07 09:54:19 -0500

    main added

---
+++ lab 6.py
@@ -0,0 +1,32 @@
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+
+def encoder(encode):
+    string = str(encode)
+    encoded_pw = ""
+    for x in string:
+        encoded_pw += str((int(x) + 3) % 10)
+    return int(encoded_pw)
+
+
+def decoder(decode):
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        menu()
+        user_option = int(input("Please enter an option: "))
+        if user_option == 1:
+            password = input("Please enter your password to encode: ")
+            encoder(password)
+            print("Your password has been encoded and stored!")
+        if user_option == 2:
+            pass
+        if user_option == 3:
+            break
